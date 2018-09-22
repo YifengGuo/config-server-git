a6bcbd781564a8580e1f230556521dd7aaa3ac63
YifengGuo <gyf940517@gmail.com>
2018-09-22 15:45:27 +0800

    test config server

---
+++ config-server/src/main/java/com/yifeng/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.yifeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by guoyifeng on 9/22/18
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
